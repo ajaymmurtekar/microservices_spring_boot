bfed5cc47308fbf59c231e57eebb8004f407f08a
Ajay Murtekar <ajaymurtekar@gmail.com>
2025-08-15 11:36:31 +0530

    added examples

---
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,26 @@
+package com.in28minutes.microservices.currencyexchangeservice.controller;
+
+import com.in28minutes.microservices.currencyexchangeservice.bean.CurrencyExchange;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = new CurrencyExchange(1001L, "USD", "INR", new BigDecimal(65));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+}
