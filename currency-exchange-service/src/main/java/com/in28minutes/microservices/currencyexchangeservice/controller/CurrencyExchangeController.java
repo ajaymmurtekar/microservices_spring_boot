6399222677c0b161ecb2d7dc89fb069eb721574f
Ajay Murtekar <ajaymurtekar@gmail.com>
2025-08-15 14:16:32 +0530

    added examples

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice.controller;
 
 import com.in28minutes.microservices.currencyexchangeservice.bean.CurrencyExchange;
+import com.in28minutes.microservices.currencyexchangeservice.repository.CurrencyExchangeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 public class CurrencyExchangeController {
+
+    @Autowired
+    private CurrencyExchangeRepository repository;
 
     @Autowired
     private Environment environment;
@@ -19,6 +24,18 @@ public class CurrencyExchangeController {
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         CurrencyExchange currencyExchange = new CurrencyExchange(1001L, "USD", "INR", new BigDecimal(65));
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+        return currencyExchange;
+    }
+
+    @GetMapping("/jpa/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValueJpa(@PathVariable String from, @PathVariable String to) {
+        CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+
+        if (currencyExchange == null) {
+            throw new RuntimeException("Unable to find data for " + from + " to " + to);
+        }
         String port = environment.getProperty("local.server.port");
         currencyExchange.setEnvironment(port);
         return currencyExchange;
---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) values (10001, 'USD', 'INR', 70.0, 'dev');
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) values (10002, 'USD', 'EUR', 0.85, 'dev');
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) values (10003, 'USD', 'GBP', 0.75, 'dev');
+insert into currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) values (10004, 'USD', 'JPY', 110.0, 'dev');
